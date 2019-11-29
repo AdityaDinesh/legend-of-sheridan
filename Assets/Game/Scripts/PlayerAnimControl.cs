b20aeb3562ca27528ddfd0a93d3ad8a979f94b9c
Aditya Dinesh <adimondinesh@gmail.com>
2019-11-29 15:52:45 -0500

    Add : Player walk and idle animations in Overworld scene

---
+++ Assets/Game/Scripts/PlayerAnimControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimControl : MonoBehaviour
+{
+    Animator playerAnim;                        // Player Animator
+    float x = 0;                                // Horizontal inputs for the Player from the User
+    float y = 0;                                // Vertical inputs for the Player from the User
+    void Start()
+    {
+        playerAnim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get horizontal and vertical inputs
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        // Set the horizontal and vertical values in the animator
+        playerAnim.SetFloat("H", x);
+        playerAnim.SetFloat("V", y);
+    }
+}
