96af4e95ccd53c9320ef038fd6c9f2071d00d6b3
shreyasbabu <shreyas.babu28@gmail.com>
2019-12-02 20:58:19 -0500

    Add : Splines for moving platform Add: Enemy 2 enemy Mechanics

---
+++ Assets/Game/Scripts/BombMan_Script.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class BombMan_Script : MonoBehaviour
+{
+    public Transform goal;
+    [SerializeField]
+    int hitPoints = 2;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        anim.SetBool("Walk", true);
+    }
+    void Update()
+    {
+
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = goal.position;
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            anim.SetBool("Attack", true);
+        }
+    }
+    void takehit()
+    {
+
+        if (hitPoints > 0)
+        {
+            hitPoints--;
+            anim.SetBool("TakeDamage", true);
+        }
+        else
+        {
+            anim.SetBool("Die", true);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Dragon_Script.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Dragon_Script : MonoBehaviour
+{
+   // public Transform goal;
+    [SerializeField]
+    int hitPoints = 2;
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = this.gameObject.GetComponent<Animator>();
+    }
+    private void Start()
+    {
+        anim.SetBool("Fly", true);
+    }
+    void Update()
+    {
+
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            anim.SetBool("Attack", true);
+        }
+    }
+    void takehit()
+    {
+
+        if (hitPoints > 0)
+        {
+            hitPoints--;
+            anim.SetBool("TakeDamage", true);
+        }
+        else
+        {
+            anim.SetBool("Die", true);
+        }
+    }
+}
