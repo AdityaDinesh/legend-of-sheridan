44f937b134a06b9f1e6b402fa9380e6e325e8472
shreyasbabu <shreyas.babu28@gmail.com>
2019-12-06 18:50:19 -0500

    Add ; Door logic

---
+++ Assets/Game/Scripts/Door_Script.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.eulerAngles = new Vector3(this.transform.eulerAngles.x, this.transform.eulerAngles.y + (30 * Time.deltaTime), this.transform.eulerAngles.z);
+
+    }
+}
