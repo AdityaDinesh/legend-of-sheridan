e67cffed19af04d7d8202cf785206ed86c9b57a8
Aditya Dinesh <adimondinesh@gmail.com>
2019-12-09 11:52:57 -0500

    no message

---
+++ Assets/PickupObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupObject : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "Player")
+        {
+            if(Input.GetButtonDown("Fire1"))
+            {
+                other.GetComponent<Animator>().SetTrigger("pickUp");
+            }
+        }
+    }
+}
