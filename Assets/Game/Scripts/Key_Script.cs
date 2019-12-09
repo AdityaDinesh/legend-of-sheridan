4039a84d7a6008efe25ff7862071004472ba9e05
Aditya Dinesh <adimondinesh@gmail.com>
2019-12-09 14:59:09 -0500

    Commiting data fetched from previous development branch

---
+++ Assets/Game/Scripts/Key_Script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key_Script : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.name == "Player")
+        {
+            //col.gameObject.GetComponent<playerScript>().hasKey = true;
+            Destroy(this.gameObject);
+        }
+    }
+
+}
